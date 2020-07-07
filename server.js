229d6bfe1b36f74b9613d49cac43b73c92d5b45a
shaygov <shaygov.it@gmail.com>
2020-07-07 20:54:02 +0300

    test

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -14,7 +13,9 @@ if(process.env.NODE_ENV === 'production') {
     //  app.get('*', (req, res) => {    res.sendfile(path.join(__dirname = 'client/build/index.html'));  })
 }
 //build mode
-app.get('*', (req, res) => {  res.sendFile(path.join(__dirname+'/client/public/index.html'));})
+app.get('*', (req, res) => {  
+    res.sendFile(path.join(__dirname+'/client/public/index.html'));
+})
 
 //start server
-app.listen(port, (req, res) => {  console.log( `server listening on port: ${port}`);})
+ app.listen(port, (req, res) => {  console.log( `server listening on port: ${port}`);});
