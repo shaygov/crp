089be52fa0691a96395f72466cdf3683b7a03a6e
shaygov <shaygov.it@gmail.com>
2020-07-07 20:58:30 +0300

    ticket shared message

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const port = process.env.PORT || 5000;
 // app.use(express.static(path.join(__dirname, 'client/build')));
 
 //production mode
-if(process.env.NODE_ENV === 'production') {  
-    app.use(express.static(path.join(__dirname, 'client/build')));  
-    //  app.get('*', (req, res) => {    res.sendfile(path.join(__dirname = 'client/build/index.html'));  })
-}
+// if(process.env.NODE_ENV === 'production') {  
+//     app.use(express.static(path.join(__dirname, 'client/build')));  
+//     //  app.get('*', (req, res) => {    res.sendfile(path.join(__dirname = 'client/build/index.html'));  })
+// }
 //build mode
 app.get('*', (req, res) => {  
     res.sendFile(path.join(__dirname+'/client/public/index.html'));
